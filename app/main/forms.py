d701f544e54a394b36c87be30f7ea470424471f7
KrapivaDA <krapivada@nashel.ru>
2025-06-29 10:17:27 +0300

    fix

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectMultipleField
-from wtforms.validators import EqualTo, Length, DataRequired
+from wtforms.validators import EqualTo, DataRequired
 
 
 # Форма для профиля
